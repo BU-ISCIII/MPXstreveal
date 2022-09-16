4e529b18fb70b3139661aa1f303b5267a903d4cc
svarona <sarai.var.fer@gmail.com>
2022-09-16 10:22:35 +0200

    Fixed config file to work with conda environment

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 [softwares]
-mybwa="YOUR PATH/bwa"
-mysamtools="YOUR PATH/samtools"
-mybamToFastq="YOUR PATH/bamToFastq"
-myseqtk="YOUR PATH/seqtk"
-myusearch="YOUR PATH/usearch"
+mybwa="bwa"
+mysamtools="samtools"
+mybamToFastq="bamToFastq"
+myseqtk="seqtk"
+myusearch="/scratch/bi/SRVCNM664_20220524_GENOMEMONKEYPOX01_anabelnegredo_S/ANALYSIS/20220527_ANALYSIS07_STR/usearch11.0.667_i86linux32"
